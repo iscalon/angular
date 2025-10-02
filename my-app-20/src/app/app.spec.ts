dad2cbd8d87e3147b7ac4f397e1c9dba99322db3
iscalon <111983483+iscalon@users.noreply.github.com>
2025-10-02 17:06:44 +0200

    [MyApp-20] Ajout ng-mocks et testing-library
    Fix App tests.

--- my-app-20/src/app/app.spec.ts
+++ my-app-20/src/app/app.spec.ts
@@ -1,10 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 import { App } from './app';
+import { MockComponents } from 'ng-mocks';
+import { Header } from './shared/components/header/header';
+import { Footer } from './shared/components/footer/footer';
+import { By } from '@angular/platform-browser';
 
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [App],
+      imports: [App, MockComponents(Header, Footer)],
     }).compileComponents();
   });
 
@@ -18,6 +22,18 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, my-app-20');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Hello');
+  });
+
+  it('should render header, footer and a router outlet', () => {
+    const fixture = TestBed.createComponent(App);
+
+    fixture.detectChanges();
+    const header = fixture.debugElement.query(By.css('app-header'));
+    expect(header).toBeTruthy();
+    const footer = fixture.debugElement.query(By.css('app-footer'));
+    expect(footer).toBeTruthy();
+    const routerOutlet = fixture.debugElement.query(By.css('router-outlet'));
+    expect(routerOutlet).toBeTruthy();
   });
 });
