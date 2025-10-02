8a3001ac5097ed4ce6510c6d28e670cb7c911ad5
iscalon <111983483+iscalon@users.noreply.github.com>
2025-10-02 17:31:16 +0200

    [MyApp-20] Ajout test inputs changeant avec project card

--- my-app-20/src/app/shared/components/project-card/project-card.spec.ts
+++ my-app-20/src/app/shared/components/project-card/project-card.spec.ts
@@ -1,23 +1,47 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RenderResult, render, screen } from '@testing-library/angular';
 import { ProjectCardComponent } from './project-card';
+import { AsyncPipe, NgOptimizedImage } from '@angular/common';
+import { MockDirective, MockProvider } from 'ng-mocks';
+import { ProjectService } from '../../../services/project-service';
+import { Project } from '../../../infrastructure/types/project';
+import { of } from 'rxjs';
+
+const mockProjects: Project[] = [
+  {
+    id: 1,
+    name: 'Project 1',
+    image: 'path-to-image1.png',
+  },
+  {
+    id: 2,
+    name: 'Project 2',
+    image: 'path-to-image2.png',
+  },
+];
 
 describe('ProjectCard', () => {
-  let component: ProjectCardComponent;
-  let fixture: ComponentFixture<ProjectCardComponent>;
+  let component: RenderResult<ProjectCardComponent>;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [ProjectCardComponent]
-    })
-    .compileComponents();
-
-    fixture = TestBed.createComponent(ProjectCardComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    component = await render(ProjectCardComponent, {
+      imports: [AsyncPipe, MockDirective(NgOptimizedImage)],
+      providers: [
+        MockProvider(ProjectService, {
+          getProject$(projectId) {
+            return of(mockProjects.find((project) => project.id === projectId)!);
+          },
+        }),
+      ],
+      inputs: {
+        projectId: 1,
+      },
+    });
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should render the project name', () => {
+    expect(screen.getByText('Project 1')).toBeTruthy();
+    component.fixture.componentRef.setInput('projectId', 2);
+    component.fixture.detectChanges();
+    expect(screen.getByText('Project 2')).toBeTruthy();
   });
 });
